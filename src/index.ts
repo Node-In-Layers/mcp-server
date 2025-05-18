766382f50619743c893da80ec684e19d3c18bbb3
Mike Cornwell <mike@mikecornwell.com>
2025-05-18 10:06:09 -0400

    feat(mcpmodels): export mcpModels

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+const name = '@node-in-layers/mcp-server'
+
+export { mcpModels } from './mcp.js'
+export { name }
 export * as mcp from './mcp.js'
 export * from './types.js'
-
-const name = '@node-in-layers/mcp-server'
-export { name }
